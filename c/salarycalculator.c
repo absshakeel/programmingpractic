492bf74e18cc9602f7325ad6bd30ada15ec93c98
absshakeel <twithabs@gmail.com>
2024-03-22 01:03:15 +0600

    salary calculator with percentage

--- c/salarycalculator.c
+++ c/salarycalculator.c
@@ -6,8 +6,10 @@ int main(){
     printf("basic_salary: ");
     scanf("%f",&basic_salary);
 
-    dearness_allowence =  basic_salary * 0.4  ;
-    house_rent =  basic_salary * 0.2  ;
+    //dearness_allowence =  basic_salary * 0.4  ;
+    dearness_allowence =  (basic_salary *  40 ) /100  ;
+    // house_rent =  basic_salary * 0.2  ;
+    house_rent =   (basic_salary *  20 ) /100  ;
     gross_salary =   basic_salary + dearness_allowence + house_rent  ;
     printf("dearness_allowence %.2f \n",dearness_allowence);
     printf("House Rent %.2f \n",house_rent);

a9834873d366eac456dbbf9667581a7036fd7eb2
absshakeel <twithabs@gmail.com>
2024-04-29 21:44:27 +0600

    getchar() and putchar()

---
+++ c/putch.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+//putchar will output character
+//getchar will input character
+int main(){
+     int ch;
+    for(ch='A';ch < 'Z';ch++){
+        putchar(ch);
+    }
+
+   /*
+   ch = getchar();
+   putchar(ch);
+   */
+
+    return 0;
+
+}

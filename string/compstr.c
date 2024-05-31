23363c510a1733934cb2164290e1a8fbf523d325
absshakeel <twithabs@gmail.com>
2024-05-31 22:55:27 +0600

    compare strig

---
+++ string/compstr.c
@@ -0,0 +1,31 @@
+
+#include <stdio.h>
+
+int compareStrings(char *str1, char *str2) {
+    while (*str1 && *str2) {
+        if (*str1 != *str2) {
+            return *str1 - *str2;
+        }
+        str1++;
+        str2++;
+    }
+    return *str1 - *str2;
+}
+
+int main() {
+    char str1[100], str2[100];
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+
+    int result = compareStrings(str1, str2);
+    if (result == 0) {
+        printf("Strings are equal.\n");
+    } else if (result < 0) {
+        printf("First string is less than second string.\n");
+    } else {
+        printf("First string is greater than second string.\n");
+    }
+    return 0;
+}
